a7d2f763a8080cf4c5467dded839280bc7434994
ekimaureen <management73690@gmail.com>
2026-07-25 11:46:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:3000/api'
-    : `${window.location.origin}/api`;  
+    : `https://back-bzf0.onrender.com/api`;  
 
 // Wait for the HTML to fully load
 document.addEventListener("DOMContentLoaded", function () {
